4de62c2c3324e22b4095559e949601bd7a357e3f
Mohammed Qureshi <mqureshi93v2@gmail.com>
2023-06-03 12:06:12 +0100

    Create, new, show and private params methods created

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,46 @@
 class ItemsController < ApplicationController
+
+  # add before action
   def index
     @items = Item.all
   end
+
+  def create
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to items_path, notice: 'Item Report Submitted'
+    else
+      render :new
+    end
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def show
+    @item.find(params[:id])
+  end
+
+
+  # add remaining methods below
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def items_params
+    params.require(:item).permit(:user_id, :name, :status, :date, :reward, :location, :description)
+  end
+
 end
