69e0319d71f9a9246b612a31e7ad0892a0ad01d7
Mohammed Qureshi <mqureshi93v2@gmail.com>
2023-06-06 21:10:32 +0100

    edit function basic

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,7 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    if @item.update(item_parans)
+    if @item.update(item_params)
       redirect_to items_path, notice: "Item succcesfully updated"
     else
       render :edit
