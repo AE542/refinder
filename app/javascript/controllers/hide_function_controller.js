6d146a1850696c44ca16fc685aa32ecedb988969
Mohammed Qureshi <mqureshi93v2@gmail.com>
2023-06-17 15:10:29 +0100

    Style changes with cards, more uniform and added fade transition to the map

--- app/javascript/controllers/hide_function_controller.js
+++ app/javascript/controllers/hide_function_controller.js
@@ -11,8 +11,25 @@ export default class extends Controller {
 
   hide() {
     // toggles the element -the map- to be hidden from view
-    this.elementTarget.classList.toggle("d-none")
+
+    //adding fade effect when button is pressed
+
+    if (document.getElementsByClassName('mapContainer')[0]) {
+      const mapContainer = document.getElementsByClassName('mapContainer')[0]
+      console.log(document.getElementsByClassName('mapContainer'))
+      if (mapContainer.style.visibility == 'hidden') {
+        mapContainer.style.visibility = 'visible';
+        mapContainer.style.opacity = '1'
+        mapContainer.style.height = '600px';
+      } else {
+        mapContainer.style.visibility = 'hidden';
+        mapContainer.style.opacity = '0';
+        mapContainer.style.height = '0';
+      }
+    }
+    //this.elementTarget.classList.toggle("d-none")
   // terniary ensures button displays correct text
     this.buttonTarget.innerText = this.buttonTarget.innerText == "Show Map" ? "Hide Map" : "Show Map"
+
   }
 }
